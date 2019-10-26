745040bbebe64e3d99d221282cb723113e996a02
Benjamin <patonacevedo@gmail.com>
2019-10-26 01:14:26 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const cors = require('cors')
+const express = require('express')
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const app = express();
+app.use(cors())
+
+const usersList = [
+    {username: 'Benjamin Ace'},
+    {username: 'TEst test'}
+]
+
+const schema = gql`
+    type Query {
+        users: [User]
+    }
+
+    type User {
+        username: String!
+    }
+`;
+
+const resolvers = {
+    Query: {
+        users: () => usersList
+    },
+};
+
+const apolloServer = new ApolloServer({
+    typeDefs: schema,
+    resolvers,
+});
+
+apolloServer.applyMiddleware({
+    app,
+    path: '/'
+});
+
+app.listen({port: 8000}, () => {
+    console.log(`Apollo server on http://localhost:8000/`);
+});
