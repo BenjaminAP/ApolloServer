b420f6e2b1d1526d900ca0e6b0846d64fa9bfc29
Benjamin <patonacevedo@gmail.com>
2019-10-27 18:33:19 -0400

    Added uuid to messages and users mutation

--- index.js
+++ index.js
@@ -1,40 +1,79 @@
 const cors = require('cors')
 const express = require('express')
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express')
+const uuidv4 = require('uuid/v4')
 
 const app = express();
 app.use(cors())
 
 const usersList = [
-    {username: 'Benjamin Ace'},
-    {username: 'TEst test'}
+    {
+        id: 1,
+        username: "Benjamin"
+    }
 ]
+
+const messages = []
 
 const schema = gql`
     type Query {
         users: [User]
+        user(id: ID): User
+        messages: [Message]
     }
 
     type Mutation {
         addUser(username: String!): [User]
+        postMessage(message: String!, userId: ID!): Message
     }
 
     type User {
+        id: ID!
         username: String!
+    }
+
+    type Message {
+        id: ID!
+        message: String!
+        user: User!
     }
 `;
 
 const resolvers = {
     Query: {
-        users: () => usersList
+        users: () => usersList,
+        user: (parent, args) => {
+            return usersList.filter( user => {
+                if (user.id == args.id) {
+                    return user
+                }
+            })
+        }
     },
     Mutation: {
         addUser: (parent, args) => {
             usersList.push({
+                id: uuidv4(),
                 username: args.username
             })
 
             return usersList
+        },
+        postMessage: (parent, args) => {
+
+            let newMessage = {
+                id: uuidv4(),
+                message: args.message,
+                user: usersList.find(user => {
+                    if (user.id == args.userId) {
+                        return user
+                    }
+                })
+            }
+            messages.push(newMessage)
+            console.log(newMessage)
+
+            return newMessage
         }
     }
 };
